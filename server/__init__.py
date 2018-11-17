a246de7bcd4dab360eb0c30f110299d5d94afc20
dariusmandres@yahoo.ca <dariusmandres@yahoo.ca>
2018-11-16 21:31:36 -0500

    made some updates

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from pathlib import Path
+import os
 
 def create_app():
 
     # creates the app
     app = Flask(__name__, static_folder='../static/dist', template_folder='../static/templates')
 
+
     # configuration
+    devConfig = Path(os.path.join(app.instance_path, 'devConfig.py'))
+    prodConfig = Path(os.path.join(app.instance_path, 'prodConfig.py'))
+
+    # dev over prod
     if devConfig.is_file():
         from instance.devConfig import DevelopmentConfig
         app.config.from_object(DevelopmentConfig)
--- server/views.py
+++ server/views.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 def index():
 
-    return render_template('index.html', test='test')
+    return render_template('index.html')

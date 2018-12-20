5c5f2b75f1126a08ec576527a717692bd31b0394
dariusmandres@yahoo.ca <dariusmandres@yahoo.ca>
2018-12-20 10:58:38 -0500

    updated custom.css

--- static/react/DemoComponent.jsx
+++ static/react/DemoComponent.jsx
@@ -7,10 +7,10 @@ class DemoComponent extends React.Component {
   render() {
     return (
       <section id="demoComponent">
-        <div className="width80">
-          <div className="flex-rw-sb-c-s m50b-s">
+        <div className="w80">
+          <div className="flex-rw-sb-c-s m50b-s p25v">
             <div>
-              <h1 className="m10b_s">Website Name</h1>
+              <h2 className="m10b_s">Website Name</h2>
             </div>
             <div>
               <a href="/" className="m10l m10r">
@@ -27,20 +27,52 @@ class DemoComponent extends React.Component {
               </a>
             </div>
           </div>
-          <div className="text-center">
-            <h2>Our services</h2>
+          <div className="m50b-s text-center">
+            <h2>Grid Layout</h2>
             <p>
               Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
               habemus commune sed, eum exerci ocurreret cu. Erant instructior ad
               per. Eum et vocent option deserunt, noster graeco nostro eos an.
               Vel mazim mediocritatem cu.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
-              habemus commune sed, eum exerci ocurreret cu. Erant instructior ad
-              per. Eum et vocent option deserunt, noster graeco nostro eos an.
-              Vel mazim mediocritatem cu.
-            </p>
+          </div>
+          <div className="flex-rw-sb-c-m m50b-s text-center">
+            <div className="flex-w25-m m10h-m m10b_s">
+              <h3>Service</h3>
+              <p>
+                Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
+                habemus commune sed, eum exerci ocurreret cu. Erant instructior
+                ad per. Eum et vocent option deserunt, noster graeco nostro eos
+                an. Vel mazim mediocritatem cu.
+              </p>
+            </div>
+            <div className="flex-w25-m m10h-m m10b_s">
+              <h3>Service</h3>
+              <p>
+                Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
+                habemus commune sed, eum exerci ocurreret cu. Erant instructior
+                ad per. Eum et vocent option deserunt, noster graeco nostro eos
+                an. Vel mazim mediocritatem cu.
+              </p>
+            </div>
+            <div className="flex-w25-m m10h-m m10b_s">
+              <h3>Service</h3>
+              <p>
+                Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
+                habemus commune sed, eum exerci ocurreret cu. Erant instructior
+                ad per. Eum et vocent option deserunt, noster graeco nostro eos
+                an. Vel mazim mediocritatem cu.
+              </p>
+            </div>
+            <div className="flex-w25-m m10h-m m10b_s">
+              <h3>Service</h3>
+              <p>
+                Lorem ipsum dolor sit amet, meis mandamus his no, no fabulas
+                habemus commune sed, eum exerci ocurreret cu. Erant instructior
+                ad per. Eum et vocent option deserunt, noster graeco nostro eos
+                an. Vel mazim mediocritatem cu.
+              </p>
+            </div>
           </div>
         </div>
       </section>
